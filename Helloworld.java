b24157d8cfc13b1cf164c17663de6ef0d6e20a25
1rex102 <158311800+1rex102@users.noreply.github.com>
2024-01-31 08:21:41 -0800

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello my name is Colton");
+        System.out.println("My favorite food is sushi");
+        System.out.println("I have one sister");
+        System.out.println("I am home schooled");
+        System.out.println("I am 16 years old");
+    }
+}
