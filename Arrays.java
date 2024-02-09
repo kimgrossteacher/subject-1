233e203e1fdf0288ac2f075c6c19bcc5176d34e4
1rex102 <158311800+1rex102@users.noreply.github.com>
2024-02-09 09:19:42 -0800

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,33 @@
+//Colton
+//My 3rd class
+//Using arrays in counting to 100
+public class Arrays {
+
+
+    public static void main(String[] args){
+    
+        int [] arrays = new int[ 100 ]; //adds another loop with counting to 100
+           for (int x=1; x<101; x++){ 
+               
+               System.out.print(x);
+               if (x%2==1){ 
+                   System.out.println(" odd"); //tells to put odd next to the odd numbers
+               }else{
+   
+   
+                   System.out.println(" even"); //tells to put even next to the even numbers
+               }
+   
+   
+           }
+           System.out.println(arrays+" is the sum"); //tells to print the sum
+           
+    }
+     
+   
+   
+   
+   }
+
+    
+
